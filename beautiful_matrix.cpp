1936835e9acff0e4bd29e2b17d71c7ecad32c9b8
dipu695 <dipupaul695@gmail.com>
2025-11-27 21:24:07 +0600

    Added solution for new problem

---
+++ beautiful_matrix.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int arr[5][5];
+    for(int i = 0 ; i < 5 ; i++)
+    {
+        for(int j = 0 ; j < 5 ; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    for(int i = 0 ; i < 5 ; i++)
+    {
+        for(int j = 0 ; j < 5 ; j++)
+        {
+            if(arr[i][j] == 1)
+            {
+                int answer = abs(i - 2) + abs(j - 2);
+                cout << answer << endl;
+            }
+        }
+    }
+
+
+return 0;
+}
